3470174bddfe69669f0bdb82a7fa7aa8312d8f0e
Aashin Mohammed A Z <145919718+mammumammi@users.noreply.github.com>
2026-07-17 22:50:28 +0530

    Add: house-robber - submission-14

---
+++ Data Structures & Algorithms/house-robber/submission-14.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+       
+        int prev1 = 0;
+        int prev2 = 0; //1,8,4,
+        for (int i = 0;i<nums.length;i++){
+            int currentMax = Math.max(prev2 + nums[i],prev1);
+            prev2 = prev1;
+            prev1 = currentMax;            
+        } 
+        return prev1;
+    }
+}
