ae78df3f22a608004402d6a09458aa95a51fa354
Aashin Mohammed A Z <145919718+mammumammi@users.noreply.github.com>
2026-08-04 07:12:46 +0530

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int currentTank = 0;
+        int tankSurplus = 0;
+        int startStation = 0;
+
+        for (int i = 0;i<gas.length;i++){
+            int netGas = gas[i] - cost[i];
+            tankSurplus += netGas;
+            currentTank += netGas;
+
+            if (currentTank < 0){
+                currentTank = 0;
+                startStation = i + 1;
+            }
+        }
+
+        return (tankSurplus >= 0) ? startStation : -1;
+    }
+}
