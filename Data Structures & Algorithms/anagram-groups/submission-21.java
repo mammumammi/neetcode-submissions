d7c8c7ac0f3cb596e05cd0e17c8c5dfcf050204b
Aashin Mohammed A Z <145919718+mammumammi@users.noreply.github.com>
2026-08-29 21:57:45 +0530

    Add: anagram-groups - submission-21

---
+++ Data Structures & Algorithms/anagram-groups/submission-21.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+
+        for (String str: strs){
+
+            char[] charArray = str.toCharArray();
+
+            Arrays.sort(charArray);
+
+            String sorted = new String(charArray);
+
+            if (map.containsKey(sorted)){
+                map.get(sorted).add(str);
+                continue;
+            }
+
+            map.put(sorted,new ArrayList<>());
+            map.get(sorted).add(str);
+
+        }
+
+        for (List<String> strings: map.values()){
+            result.add(strings);
+        }
+
+        return result;
+    }
+}
