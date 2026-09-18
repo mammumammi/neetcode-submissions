30b41a6311bc80b49e5c7031ffa975bc7a82ce7d
Aashin Mohammed A Z <145919718+mammumammi@users.noreply.github.com>
2026-09-18 16:46:51 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Deque<Integer> openStack = new ArrayDeque<>();
+        Deque<Integer> starStack = new ArrayDeque<>();
+
+        for (int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if (ch == '('){
+                openStack.push(i);
+            }
+            else if (ch == '*') starStack.push(i);
+            else{
+                if (!openStack.isEmpty()){
+                    openStack.pop();
+                }
+                else if (!starStack.isEmpty()) starStack.pop();
+                else return false;
+            }
+        }
+
+        while (!openStack.isEmpty() && !starStack.isEmpty()){
+            if (openStack.pop() > starStack.pop()) return false;
+        }
+
+        return openStack.isEmpty();
+    }
+}
