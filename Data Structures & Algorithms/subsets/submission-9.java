e70893503146f9edb3b640885732e4d48897d823
Aashin Mohammed A Z <145919718+mammumammi@users.noreply.github.com>
2026-09-21 21:37:22 +0530

    Add: subsets - submission-9

---
+++ Data Structures & Algorithms/subsets/submission-9.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        //backtracking
+        // [], [1], [1,2] , [1,2,3], [1,3],[2]
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0,nums,new ArrayList<>(),result);
+        return result;
+    }
+
+    private void backtrack(int start,int[] nums,List<Integer> current,List<List<Integer>> result){
+        //[]
+        result.add(new ArrayList<>(current));
+
+        //[] -> [1],[1,2],[1,,]
+
+        for (int i = start;i<nums.length;i++){
+            current.add(nums[i]);
+            backtrack(i+1,nums,current,result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
